832721273c0734b8930ca1a7a2a02bcb625ac990
张峻锋 <SternWZhang@outlook.com>
2015-09-20 00:23:55 +0800

    Add ring queue project

---
+++ cpp/project/ring_queue/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
+#include "ring_queue.h"
+
+using namespace std;
+
+int main(){
+    ring_queue<int> s(10);
+    for(int i=0;i<10;i++)
+        s.push(i);
+    for(int i=0;i<10;i++){
+        auto t = s.fetch();
+        if(t != nullptr)
+            cout << *t << endl;
+        else
+            cout << "t is none" << endl;
+    }
+    return 0;
+}
+
---
+++ cpp/project/ring_queue/untitled1.pro
@@ -0,0 +1,17 @@
+TEMPLATE = app
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+    main.cpp
+
+
+QMAKE_CXXFLAGS += -std=c++11
+
+include(deployment.pri)
+qtcAddDeployment()
+
+HEADERS += \
+    ring_queue.h
+
