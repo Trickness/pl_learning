5f37247f576cec8d86359a5c64ed4386914b5b6b
trickness <sternwzhang@outlook.com>
2015-09-24 23:23:29 +0800

    make_ring_queue

--- cpp/project/ring_queue/main.cpp
+++ cpp/project/ring_queue/main.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 int main(){
+    auto shared_queue = make_ring_queue_writer<int>(23);
     ring_queue<int> s(10);
     for(int i=0;i<10;i++)
         s.push(i);
--- cpp/project/ring_queue/ring_queue.h
+++ cpp/project/ring_queue/ring_queue.h
@@ -8,11 +8,13 @@ namespace{
 }
 
 template<typename T>
-std::weak_ptr<T>    make_ring_queue_reader(std::shared_ptr& writer){
+std::weak_ptr<class ring_queue<T>>  make_ring_queue_reader(std::shared_ptr<T>& writer){
+    return std::weak_ptr<T>(writer);
 }
 
 template<typename T>
-std::smart_ptr<T>   make_ring_queue_writer(int queue_size){
+std::shared_ptr<class ring_queue<T>>   make_ring_queue_writer(int queue_size){
+    return std::make_shared<class ring_queue<T>>(queue_size);
 }
 
 #endif // RING_QUEUE_H
