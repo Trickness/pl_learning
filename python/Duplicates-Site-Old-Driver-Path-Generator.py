ecfa4a333de515f826faaff88eed78152e78c7c0
trickness <sternwzhang@outlook.com>
2016-03-26 23:06:19 +0800

    changes

--- python/Duplicates-Site-Old-Driver-Path-Generator.py
+++ python/Duplicates-Site-Old-Driver-Path-Generator.py
@@ -9,11 +9,8 @@ if (len(str_url) == 0):
 
 str_headers = {'User-Agent':'Mozilla/5.0 (Windows NT 100.1; WOW64; rv:29.0) Gecko/20160101 Firefox/49.0'}
 
-try:
-    req = urllib.request.Request(url=str_url, headers=str_headers);
-except urllib.error.HTTPError as e:
-    print ("%s"%e);
-    exit();
+req = urllib.request.Request(url=str_url, headers=str_headers);
+
 try:
     html = urllib.request.urlopen(req)
 except urllib.error.HTTPError as e:
