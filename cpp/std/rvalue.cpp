b9ee564b6a43c573758a1a52072fdea66b70dbef
trickness <sternwzhang@outlook.com>
2017-02-28 14:57:02 +0800

    rvalue

---
+++ cpp/std/rvalue.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int g_constructCount = 0;
+int g_copyConstructCount = 0;
+int g_destructCount = 0;
+struct A{
+    public:
+        A():m_ptr(new int(0)){
+            cout << "Construct" << endl;
+        }
+        // 深度拷贝
+        //A(const A& a):m_ptr(new int(*a.m_ptr)){
+        //    cout << "Copy Construct" << endl;
+        //}
+        // 浅度拷贝
+        A(const A& a):m_ptr(a.m_ptr){
+        }
+        ~A(){
+            delete m_ptr;
+        }
+    private:
+        int *m_ptr;
+};
+
+A GetA(){
+    return A();
+}
+
+int main(){
+    A a = GetA();
+    return 0;
+}
